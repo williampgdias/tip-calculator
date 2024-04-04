3d2ae5d18b61f422834db4c4e955e4464b3575a0
William Dias <smdwilliam89@gmail.com>
2024-04-04 12:33:33 -0300

    feat(index.js): Declaring the default variables

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,9 @@ const amountBill = document.getElementsByClassName('total-bill');
 const numberOfPeople = document.getElementById('totalPeopleInput');
 const numberOfTips = document.querySelectorAll('.tip-amount');
 const btnSend = document.getElementById('btnSend');
+
+// Declaring the default variables
+let pressedNumber = '';
+let isPressed = false;
+let pressedBtnTips = numberOfTips;
+let pressedIndex = -1;
