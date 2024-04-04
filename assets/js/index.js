1207748298a4d13729b92e2977448e2e3c02de50
William Dias <smdwilliam89@gmail.com>
2024-04-04 12:34:17 -0300

    feat(index.js): Add the function that handles numeric input events and ensures input validation.

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,3 +8,24 @@ let pressedNumber = '';
 let isPressed = false;
 let pressedBtnTips = numberOfTips;
 let pressedIndex = -1;
+
+/**
+ * Handles numeric input events and ensures input validation.
+ *
+ * This function takes an event object as a parameter, typically representing a keyboard event.
+ * It checks if the input is numeric and allows decimal point '.' only once.
+ * When 'Backspace' is pressed, it removes the last character from the input.
+ *
+ * @param {Event} e The event object representing a keyboard input event.
+ * @returns {void} This function does not return a value, but it modifies the 'pressedNumber' variable.
+ */
+function handleNumericInput(e) {
+    if (
+        /^\d$|\.$/.test(e.key) ||
+        (e.key === '.' && !pressedNumber.includes('.'))
+    ) {
+        pressedNumber += e.key;
+    } else if (e.key == 'Backspace') {
+        pressedNumber = pressedNumber.slice(0, -1);
+    }
+}
