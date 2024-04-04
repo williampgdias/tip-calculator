6cae0a13958539d83c4e2924170d5eb8984b0608
William Dias <smdwilliam89@gmail.com>
2024-04-04 12:33:04 -0300

    feat(index.js): Capturing the element variables.

---
+++ assets/js/index.js
@@ -0,0 +1,4 @@
+const amountBill = document.getElementsByClassName('total-bill');
+const numberOfPeople = document.getElementById('totalPeopleInput');
+const numberOfTips = document.querySelectorAll('.tip-amount');
+const btnSend = document.getElementById('btnSend');
